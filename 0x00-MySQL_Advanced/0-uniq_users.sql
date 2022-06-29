a8cc073de49084e09697a09ac6bd79772bc37cba
olart <akanbi.ges201@gmail.com>
2022-06-29 17:36:58 +0100

    task 0 and mini readme

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,18 @@
+-- this script creates a table users with certain requirements
+-- With these attributes:
+-- 	id, integer, never null, auto increment and primary key
+--	email, string (255 characters), never null and unique
+--	name, string (255 characters)
+-- If the table already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS users
+(
+        id INT AUTO_INCREMENT,
+
+        email VARCHAR(255) NOT NULL UNIQUE,
+
+        name VARCHAR(255),
+
+        PRIMARY KEY(id)
+
+);
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,11 @@
+<h1>Learning Objectives</h1>
+<p>At the end of this project, you are expected to be able to explain to anyone, <b>without the help of Google<b>:
+<br>
+<h2>General</h2>
+<ul>
+<li>How to create tables with constraints</li>
+<li>How to optimize queries by adding indexes</li>
+<li>What is and how to implement stored procedures and functions in MySQL</li>
+<li>What is and how to implement views in MySQL</li>
+<li>What is and how to implement triggers in MySQL</li>
+</ul>
