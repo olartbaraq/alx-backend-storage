0cbf57346d5f9f498a5105782c6d75b41e0ed0ef
olart <akanbi.ges201@gmail.com>
2022-07-06 18:30:12 +0100

    added pep 8, corrected the store method by putting the self arg

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+text file to write strings into Redis,
+create clss and instantiate, add methods
+"""
+
+from typing import Any, Union
+import uuid
+import redis
+
+
+class Cache():
+    """
+    Cache class to store Redis client instance
+    """
+    def __init__(self):
+        """instantiation method to store Redis
+        client varaibles
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        """method to return a string from any argument"""
+        random_key = str(uuid.uuid4())
+        self._redis.mset({random_key: data})
+        #result = _redis.get(random_key).decode('utf-8')
+        return random_key
