a63b62349d362fc106e0dc679447afb7cd527254
olart <akanbi.ges201@gmail.com>
2022-07-06 22:19:59 +0100

    input and output list keys

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,7 +11,7 @@ import uuid
 
 
 def count_calls(method: Callable) -> Callable:
-    """Decortator for counting how many times a function
+    """Decorator for counting how many times a function
     has been called """
     key = method.__qualname__
 
@@ -21,6 +21,22 @@ def count_calls(method: Callable) -> Callable:
         self._redis.incr(key)
         return method(self, *args, **kwargs)
     return raise_count
+
+
+def call_history(method: Callable) -> Callable:
+    """Decorator for storing the history of inputs
+    and outputs for a particular function"""
+    @wraps(method)
+    def wrap_history(*args):
+        """Wrapper for decorator functionality """
+        inputlist = method.__qualname__ + ":inputs"
+        outputlist = method.__qualname__ + ":outputs"
+        cache = (args[0])._redis
+        cache.rpush(inputlist, args[1])
+        output = method(*args)
+        cache.rpush(outputlist, output)
+        return output
+    return wrap_history
 
 
 class Cache():
@@ -35,6 +51,7 @@ class Cache():
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """method to return a string from any argument"""
         random_key = str(uuid.uuid4())
