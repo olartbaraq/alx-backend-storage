d627de6399a2a008fe08ba27481123f267eefbc7
olart <akanbi.ges201@gmail.com>
2022-07-06 18:35:47 +0100

    added the correct syntax for setting key and value

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,6 +23,5 @@ class Cache():
     def store(self, data: Any) -> str:
         """method to return a string from any argument"""
         random_key = str(uuid.uuid4())
-        self._redis.mset({random_key: data})
-        #result = _redis.get(random_key).decode('utf-8')
+        self._redis.set(random_key, data)
         return random_key
